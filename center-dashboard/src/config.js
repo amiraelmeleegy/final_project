9bf83f6005c875f22e1dc95efad51f1086fc4cdd
amira <amiraelmeleegy56@gmail.com>
2026-05-03 01:09:28 +0300

    update backend config

--- center-dashboard/src/config.js
+++ center-dashboard/src/config.js
@@ -1,2 +1,3 @@
-export const API_URL = "http://192.168.1.18:5555";
+// export const API_URL = "http://192.168.1.18:5555";
+export const API_URL = "https://yourappname-production.up.railway.app/";
 export const CENTER_ID = "center1";
